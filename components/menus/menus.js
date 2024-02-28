6b39016d9d1b374c35ab49eaf11a80b9cd11ac93
whz <gebixiaowang@foxmail.com>
2024-02-29 00:18:59 +0800

    优化

--- components/menus/menus.js
+++ components/menus/menus.js
@@ -4,7 +4,9 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    oneList: ["功能A","功能B","功能C","功能D"],
+    twoList: ["功能E","功能F","功能G","功能H"],
+    threeList: ["功能I","功能J","",""],
   },
 
   /**
